44be068dff49a043f4acac31dea03217c9701b21
Muhammed Yiğit Demirci <muhammedyigitdemirci@gmail.com>
2026-02-12 20:51:03 +0300

    Add Featurebase SDK and remove support chat

---
+++ featurebase.js
@@ -0,0 +1,23 @@
+// featurebase.js
+!(function(e,t){
+  var a="featurebase-sdk";
+  function n(){
+    if(!t.getElementById(a)){
+      var e=t.createElement("script");
+      e.id=a;
+      e.src="https://do.featurebase.app/js/sdk.js";
+      t.getElementsByTagName("script")[0].parentNode.insertBefore(e,t.getElementsByTagName("script")[0]);
+    }
+  }
+  "function"!=typeof e.Featurebase && (e.Featurebase=function(){(e.Featurebase.q=e.Featurebase.q||[]).push(arguments)});
+  "complete"===t.readyState||"interactive"===t.readyState ? n() : t.addEventListener("DOMContentLoaded",n);
+})(window,document);
+
+Featurebase("boot", {
+  appId: "698df5c653f344e7ed78253c",
+  email: "user@example.com",
+  userId: "12345",
+  createdAt: new Date().toISOString(),
+  theme: "dark",
+  language: "en"
+});
